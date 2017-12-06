28d976b2b7ca97377ab4f569bda684d01f8c836d
Mattreal <TheRealA@web.de>
2017-12-06 06:43:28 +0100

    6th of 2017

--- 2017/06_1.py
+++ 2017/06_1.py
@@ -1,12 +1,31 @@
 def getLines(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
+		return [int(i) for i in [line.strip().split('\t') for line in f][0]]
 	
 def main():
 	#lines = [l.split(' ') for l in getLines('06_0'.txt)]
-	lines = getLines('06_0.txt')
+	seen = []
+	line = getLines('06_0.txt')
+	line = [0,2,7,0]
 
-	print lines	
+	while not line in seen:
+		seen.append(line)
+		line = [i for i in line]
+		mI = line.index(max(line))
+		x = line[mI]
+		line[mI] = 0
+		i = mI + 1
+		while x > 0:
+			i %= len(line)
+			if x / (len(line) - 1) < 0:
+				line[i] = x;
+				x = 0
+			else:
+				line[i] += 1
+				x -= 1
+			i += 1
+
+	print len(seen)
 
 if __name__ == '__main__':
 	main()
