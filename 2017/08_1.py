ab337adff7c05c3b41464f573c4cd7d70b8dae1c
Mattreal <TheRealA@web.de>
2017-12-08 06:59:37 +0100

    On the 8th of 2017

--- 2017/08_1.py
+++ 2017/08_1.py
@@ -1,12 +1,37 @@
+class Instruction:
+	def __init__(self, register, value, condition):
+		self.register = register
+		self.value = value
+		self.condition = condition
+	def executeIfTrue(self, registers):
+		parts = self.condition.split(' ')
+		if not parts[0] in registers:
+			registers[parts[0]] = 0
+		if not self.register in registers:
+			registers[self.register] = 0			
+		parts[0] = registers[parts[0]]
+		print ' '.join([str(p) for p in parts])
+		if eval(' '.join([str(p) for p in parts])):
+			registers[self.register] += self.value
+
+def getInstruction(line):
+	parts = line.split(' ')
+	value = (-1 if parts[1] == 'dec' else 1) * int(parts[2])
+	return Instruction(parts[0], value, ' '.join(parts[4:]))
+
 def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
-	
+
+
 def main():
 	#lines = [l.split(' ') for l in getLines('08_0'.txt)]
 	lines = getLines('08_0.txt')
-
-	print lines	
+	registers = {}
+	instructions = [getInstruction(l) for l in lines]
+	for i in instructions:
+		i.executeIfTrue(registers)
+	print registers[max(registers, key=lambda x: registers.get(x))]
 
 if __name__ == '__main__':
 	main()
--- 2017/08_2.py
+++ 2017/08_2.py
@@ -1,12 +1,48 @@
+class Instruction:
+	def __init__(self, register, value, condition):
+		self.register = register
+		self.value = value
+		self.condition = condition
+	def executeIfTrue(self, registers):
+		parts = self.condition.split(' ')
+		if not parts[0] in registers:
+			registers[parts[0]] = 0
+		if not self.register in registers:
+			registers[self.register] = 0			
+		parts[0] = registers[parts[0]]
+		if eval(' '.join([str(p) for p in parts])):
+			registers[self.register] += self.value
+
+def getInstruction(line):
+	parts = line.split(' ')
+	value = (-1 if parts[1] == 'dec' else 1) * int(parts[2])
+	return Instruction(parts[0], value, ' '.join(parts[4:]))
+
 def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
-	
+
+
 def main():
 	#lines = [l.split(' ') for l in getLines('08_0'.txt)]
 	lines = getLines('08_0.txt')
-
-	print lines	
+	history=[]
+	registers = {}
+	history.append(registers)
+	instructions = [getInstruction(l) for l in lines]
+	for i in instructions:
+		registers = dict(registers)
+		i.executeIfTrue(registers)
+		history.append(registers)
+	
+	m = 0
+	for r in history:
+		if len(r) <= 0:
+			continue
+		key = max(r, key=lambda x: r.get(x))
+		if r[key] > m:
+			m = r[key]
+	print m
 
 if __name__ == '__main__':
 	main()
