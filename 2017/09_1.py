c45c9a5799bed0ac03d117f7c1e65834e1b46319
Mattreal <TheRealA@web.de>
2017-12-09 06:28:37 +0100

    On the 9th of 2017

--- 2017/09_1.py
+++ 2017/09_1.py
@@ -1,12 +1,41 @@
 def getLines(path):
+	#return "{{<!>},{<!>},{<!>},{<a>}}"
 	with open(path, 'r') as f:
+		return f.readline().strip()
 		return [line.strip() for line in f]
 	
 def main():
 	#lines = [l.split(' ') for l in getLines('09_0'.txt)]
-	lines = getLines('09_0.txt')
+	line = getLines('09_0.txt')
+	nextIgnore = False
+	groupIndex = 0
+	groups = []
+	openAngle = False
+	for c in line:
+		if nextIgnore:
+			nextIgnore = False
+			continue
+		if c == '!':
+			nextIgnore = True
+			continue
+		if openAngle:
+			if c == '>':
+				openAngle = False
+				continue
+			continue
+		if c == '<':
+			openAngle = True
+			continue
+		if c == '{':
+			groupIndex += 1
+			continue
+		if c == '}':
+			if groupIndex > 0:
+				groups.append(groupIndex)
+				groupIndex -= 1
+				continue
 
-	print lines	
+	print sum(groups)	
 
 if __name__ == '__main__':
 	main()
--- 2017/09_2.py
+++ 2017/09_2.py
@@ -1,12 +1,43 @@
 def getLines(path):
+	#return "<{o\"i!a,<{i<a>"
 	with open(path, 'r') as f:
+		return f.readline().strip()
 		return [line.strip() for line in f]
 	
 def main():
 	#lines = [l.split(' ') for l in getLines('09_0'.txt)]
-	lines = getLines('09_0.txt')
+	line = getLines('09_0.txt')
+	nextIgnore = False
+	groupIndex = 0
+	groups = []
+	garbage = []
+	openAngle = False
+	for c in line:
+		if nextIgnore:
+			nextIgnore = False
+			continue
+		if c == '!':
+			nextIgnore = True
+			continue
+		if openAngle:
+			if c == '>':
+				openAngle = False
+				continue
+			garbage.append(c)
+			continue
+		if c == '<':
+			openAngle = True
+			continue
+		if c == '{':
+			groupIndex += 1
+			continue
+		if c == '}':
+			if groupIndex > 0:
+				groups.append(groupIndex)
+				groupIndex -= 1
+				continue
 
-	print lines	
+	print garbage, len(garbage)	
 
 if __name__ == '__main__':
 	main()
