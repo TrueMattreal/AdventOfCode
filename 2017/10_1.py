0bdf22cb0c5c75671d146678ae8b0531f6ee45b0
Mattreal <TheRealA@web.de>
2017-12-10 08:20:38 +0100

    On the 10th of 2017

--- 2017/10_1.py
+++ 2017/10_1.py
@@ -1,12 +1,40 @@
 def getLines(path):
+	#return [3, 4, 1, 5]
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
+		return map(int, f.readline().strip().split(','))
 	
+def getInput(path):
+	with open(path, 'r') as f:
+		return  f.readline().strip()
+
+def getSublist(l, s, e):
+	return [l[i%len(l)] for i in xrange(s, s + e)]
+
+def setSublist(l, s, sl):
+	newList = list(l)
+	for i in xrange(len(sl)):
+		newList[(s + i)%len(newList)] = sl[i]
+	return newList
+
 def main():
 	#lines = [l.split(' ') for l in getLines('10_0'.txt)]
-	lines = getLines('10_0.txt')
+	lengths = getLines('10_0.txt')
+	numbers = [i for i in xrange(256)]
+	skip = 0
+	i = 0
+	numbersList = [numbers]
+	for length in lengths:
+		sublist = getSublist(numbers, i , length)
+		sublist = sublist[::-1]
+		numbers = setSublist(numbers, i, sublist)
+		numbersList.append(numbers)
 
-	print lines	
+		i += length + skip
+		i %= len(numbers)
+		skip += 1
+
+	solution = numbersList[-1]
+	print solution[0] * solution[1], solution
 
 if __name__ == '__main__':
 	main()
--- 2017/10_2.py
+++ 2017/10_2.py
@@ -1,12 +1,38 @@
-def getLines(path):
+def getLengths(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
-	
-def main():
-	#lines = [l.split(' ') for l in getLines('10_0'.txt)]
-	lines = getLines('10_0.txt')
+		return  f.readline().strip()
 
-	print lines	
+def getSublist(l, s, e):
+	return [l[i%len(l)] for i in xrange(s, s + e)]
+
+def setSublist(l, s, sl):
+	newList = list(l)
+	for i in xrange(len(sl)):
+		newList[(s + i)%len(newList)] = sl[i]
+	return newList
+
+def main():
+	lengths = [ord(c) for c in getLengths('10_0.txt')]
+	lengths.extend([17,31,73,47,23])
+	input = [i for i in xrange(256)]
+	skip = 0
+	i = 0
+	for round in xrange(64):
+		for length in lengths:
+			sublist = getSublist(input, i , length)
+			sublist = sublist[::-1]
+			input = setSublist(input, i, sublist)
+
+			i += length + skip
+			i %= len(input)
+			skip += 1
+	solution = input
+	print solution
+	denseHash = []
+	for i in xrange(16):
+		denseHash.append(reduce(lambda x,y: x^y, solution[16*i:16*i+16]))
+
+	print ''.join([hex(h)[2:].zfill(2) for h in denseHash])
 
 if __name__ == '__main__':
 	main()
