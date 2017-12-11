3316f9d60949d39b10da92bb4acd5d4e6cfed659
Mattreal <TheRealA@web.de>
2017-12-11 06:15:08 +0100

    On the 11th of December

--- 2017/11_1.py
+++ 2017/11_1.py
@@ -1,12 +1,32 @@
 def getLines(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
-	
-def main():
-	#lines = [l.split(' ') for l in getLines('11_0'.txt)]
-	lines = getLines('11_0.txt')
+		return f.readline().strip().split(',')
 
-	print lines	
+def main():
+	cmds = getLines('11_0.txt')
+	y = 0
+	x = 0
+	for cmd in cmds:
+		if cmd == 'n':
+			y+=1
+		if cmd == 's':
+			y-=1
+		if cmd == 'nw':
+			y+=0.5
+			x-=0.5
+		if cmd == 'ne':
+			y+=0.5
+			x+=0.5
+		if cmd == 'sw':
+			y-=0.5
+			x-=0.5
+		if cmd == 'se':
+			y-=0.5
+			x+=0.5
+	print x, y	
+	# it was faster to calculate the rest (even if it was only three lines as you can see in 11_2)
+	# so I got rank 45 :)
+# 460 + 225
 
 if __name__ == '__main__':
 	main()
--- 2017/11_2.py
+++ 2017/11_2.py
@@ -1,12 +1,40 @@
 def getLines(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
-	
-def main():
-	#lines = [l.split(' ') for l in getLines('11_0'.txt)]
-	lines = getLines('11_0.txt')
+		return f.readline().strip().split(',')
 
-	print lines	
+def getDistance(x,y):
+	dis = x / 0.5
+	i = y - x
+	dis += i
+	return dis
+
+def main():
+	cmds = getLines('11_0.txt')
+	y = 0
+	x = 0
+	m = 0
+	for cmd in cmds:
+		if cmd == 'n':
+			y+=1
+		if cmd == 's':
+			y-=1
+		if cmd == 'nw':
+			y+=0.5
+			x-=0.5
+		if cmd == 'ne':
+			y+=0.5
+			x+=0.5
+		if cmd == 'sw':
+			y-=0.5
+			x-=0.5
+		if cmd == 'se':
+			y-=0.5
+			x+=0.5
+		dis = getDistance(x, y)
+		if dis > m:
+			m = dis
+			print x, y
+	print m
 
 if __name__ == '__main__':
 	main()
