b281f71f79527572e674171cb2c4eadf08787d6e
Mattreal <TheRealA@web.de>
2017-12-12 06:30:07 +0100

    On the 12th of 2017

--- 2017/12_1.py
+++ 2017/12_1.py
@@ -2,11 +2,34 @@ def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
 	
+class Program:
+	def __init__(self, id, contains):
+		self.id = id
+		self.contains = contains
+
 def main():
 	#lines = [l.split(' ') for l in getLines('12_0'.txt)]
 	lines = getLines('12_0.txt')
+	programs = []
+	connectedToZero = []
+	for line in lines:
+		(id, connected) = line.split('<->', 1)
+		connected = [int(c.strip()) for c in connected.split(',')]
+		programs.append(Program(int(id), connected))
+	length = 0
+	connectedToZero.append(programs[0])
+	while len(connectedToZero) != length:
+		print length, len(connectedToZero)
+		length = len(connectedToZero)
+		for line in programs:
+			if line in connectedToZero:
+				continue
+			for c in connectedToZero:
+				if line.id in c.contains:
+					connectedToZero.append(line)
+					break
 
-	print lines	
+	print len(connectedToZero)	
 
 if __name__ == '__main__':
 	main()
--- 2017/12_2.py
+++ 2017/12_2.py
@@ -2,11 +2,39 @@ def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
 	
+class Program:
+	def __init__(self, id, contains):
+		self.id = id
+		self.contains = contains
+
 def main():
 	#lines = [l.split(' ') for l in getLines('12_0'.txt)]
 	lines = getLines('12_0.txt')
+	programs = []
+	groups = []
+	for line in lines:
+		(id, connected) = line.split('<->', 1)
+		connected = [int(c.strip()) for c in connected.split(',')]
+		programs.append(Program(int(id), connected))
+	length = 0
+	groups.append([programs[0]])
+	programs.pop(0)
+	while len(programs) > 0:
+		if len(groups[-1]) != length:
+			length = len(groups[-1])
+		else:
+			groups.append([programs[0]])
+			programs.pop(0)
 
-	print lines	
+		for i in range(20):
+			for p in programs:
+				for c in groups[-1]:
+					if p.id in c.contains or c.id in p.contains:
+						groups[-1].append(p)
+						programs.pop(programs.index(p))
+						break
+	print len(groups), [len(g) for g in groups]
+
 
 if __name__ == '__main__':
 	main()
