01d487ed32b976b53e8e02595a27bdff4e25d2c3
Mattreal <TheRealA@web.de>
2017-12-16 09:51:23 +0100

    On the 16th of 2017

--- 2017/16_1.py
+++ 2017/16_1.py
@@ -1,12 +1,26 @@
-def getLines(path):
+def getCmds(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
+		return f.readline().strip().split(',')
 	
 def main():
-	#lines = [l.split(' ') for l in getLines('16_0'.txt)]
-	lines = getLines('16_0.txt')
-
-	print lines	
+	lines = getCmds('16_0.txt')
+	programs = list("abcdefghijklmnop")
+	for line in lines:
+		move = line[0]
+		params = line[1:].split('/')
+		if move == 's':
+			i = int(params[0])
+			programs = programs[-i:] + programs[:len(programs)-i]
+		elif move == 'p':
+			i = programs.index(params[0])
+			j = programs.index(params[1])
+			programs[i] = params[1]
+			programs[j] = params[0]
+		elif move == 'x':
+			i = int(params[0])
+			j = int(params[1])
+			programs[i], programs[j] = programs[j], programs[i]
+	print ''.join(programs)
 
 if __name__ == '__main__':
 	main()
--- 2017/16_2.py
+++ 2017/16_2.py
@@ -1,12 +1,31 @@
-def getLines(path):
+# needs some time
+
+def getCmds(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
+		return f.readline().strip().split(',')
 	
 def main():
-	#lines = [l.split(' ') for l in getLines('16_0'.txt)]
-	lines = getLines('16_0.txt')
-
-	print lines	
+	lines = getCmds('16_0.txt')
+	programs = list("abcdefghijklmnop")
+	for i in xrange(1000000000):
+		line = lines[i%len(lines)]
+		if i % 1000000 == 0:
+			print i
+		move = line[0]
+		params = line[1:].split('/')
+		if move == 's':
+			i = int(params[0])
+			programs = programs[-i:] + programs[:len(programs)-i]
+		elif move == 'p':
+			i = programs.index(params[0])
+			j = programs.index(params[1])
+			programs[i] = params[1]
+			programs[j] = params[0]
+		elif move == 'x':
+			i = int(params[0])
+			j = int(params[1])
+			programs[i], programs[j] = programs[j], programs[i]
+	print ''.join(programs)
 
 if __name__ == '__main__':
 	main()
