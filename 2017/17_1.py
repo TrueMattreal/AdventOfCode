f874a78a30c88ab6b9a68af1ef7d141efb4a5843
Mattreal <TheRealA@web.de>
2018-11-29 18:11:47 +0100

    get ready for 2018
    - created 2018 directory
    - upgraded generate script for python 3.x

--- 2017/17_1.py
+++ 2017/17_1.py
@@ -1,12 +1,62 @@
 def getLines(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
+		return [line.strip().split(' ') for line in f]
 	
 def main():
 	#lines = [l.split(' ') for l in getLines('17_0'.txt)]
 	lines = getLines('17_0.txt')
+	skip = 0
+	registers = {}
+	sound = []
+	alpha = "abcdefghijklmnopqrstuvwxyz"
+	for c in alpha[:17]:
+		registers[c] = 0
+	i = -1
+	while i < len(lines):
 
-	print lines	
+		i += 1
+		line = lines[i]
+		cmd = line[0]
+		if cmd == 'set':
+			if line[2] in alpha:
+				registers[line[1]] = registers[(line[2])]
+			else:
+				registers[line[1]] = int(line[2])
+		elif cmd == 'add':
+			if line[2] in alpha:
+				registers[line[1]] += registers[line[2]]
+			else:
+				registers[line[1]] += int(line[2])
+		elif cmd == 'mul':
+			if line[2] in alpha:
+				registers[line[1]] *= registers[line[2]]
+			else:
+				registers[line[1]] *= int(line[2])
+		elif cmd == 'mod':
+			if line[2] in alpha:
+				registers[line[1]] %= registers[line[2]]
+			else:
+				registers[line[1]] %= int(line[2])
+		elif cmd == 'rcv' and registers[line[1]] != 0:
+			if len(sound) > 1: 
+				sound.append(sound[-2])
+		elif cmd == 'snd':
+			sound.append(registers[line[1]])
+		elif cmd == 'jgz':
+			pos = 1
+			if line[2] in alpha and registers[line[2]] < 0:
+				pos = -1
+			elif line[2] not in alpha and int(line[2]) > 0:
+				pos = -1
+
+			if line[1] in alpha:
+				i += registers[(line[1])] * pos
+			else:
+				i += int(line[1]) * pos
+			if pos < 0:
+				i -= 1
+
+	print sound, registers
 
 if __name__ == '__main__':
 	main()
---
+++ 2018/10_1.py
@@ -0,0 +1,12 @@
+def getLines(path):
+	with open(path, 'r') as f:
+		return [line.strip() for line in f]
+	
+def main():
+	#lines = [l.split(' ') for l in getLines('10_0'.txt)]
+	lines = getLines('10_0.txt')
+
+	print(lines)
+
+if __name__ == '__main__':
+	main()
