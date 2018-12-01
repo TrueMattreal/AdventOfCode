3de0c7f2f696f08c728cbe18e8a04791e501fbcd
Mattreal <TheRealA@web.de>
2018-12-01 06:08:48 +0100

    2018-12-01

--- 2018/01_1.py
+++ 2018/01_1.py
@@ -1,12 +1,17 @@
 def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
-	
+
 def main():
 	#lines = [l.split(' ') for l in getLines('01_0'.txt)]
 	lines = getLines('01_0.txt')
-
-	print(lines)
+	freq = 0
+	for l in lines:
+		if l[0] == '+':
+			freq += int(l[1:])
+		else:
+			freq -= int(l[1:])
+	print(freq)
 
 if __name__ == '__main__':
 	main()
--- 2018/01_2.py
+++ 2018/01_2.py
@@ -1,12 +1,23 @@
 def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
-	
+
 def main():
 	#lines = [l.split(' ') for l in getLines('01_0'.txt)]
 	lines = getLines('01_0.txt')
-
-	print(lines)
+	freq = 0
+	freqs = [0]
+	lines = ['+3', '+3', '+4', '-2', '-4']
+	while True:
+		for l in lines:
+			if l[0] == '+':
+				freq += int(l[1:])
+			else:
+				freq -= int(l[1:])
+			if (freq in freqs):
+				print(freq)
+				return
+			freqs.append(freq)
 
 if __name__ == '__main__':
 	main()
