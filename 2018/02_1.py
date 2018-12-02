9209b5cdb163ad038b03ea6435820e15e8724282
Mattreal <TheRealA@web.de>
2018-12-02 08:06:47 +0100

    2018-12-02

--- 2018/02_1.py
+++ 2018/02_1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
@@ -5,8 +7,30 @@ def getLines(path):
 def main():
 	#lines = [l.split(' ') for l in getLines('02_0'.txt)]
 	lines = getLines('02_0.txt')
+	allCounts = {}
+	for l in lines:
+		counts = Counter(l)
+		already = []
+		linesCounts ={}
+		for key in counts:
+			times = counts[key]
+			if times in already or times == 1:
+				continue
+			if times not in linesCounts:
+				linesCounts[times] = 0
+			linesCounts[times] += 1	
+			already.append(times)
 
-	print(lines)
+		print(allCounts)
+		for key in linesCounts:
+			if key not in allCounts:
+				allCounts[key] = 0
+			allCounts[key] += 1
+
+	checksum = 1
+	for key in allCounts:
+		checksum *= allCounts[key]
+	print(checksum)
 
 if __name__ == '__main__':
 	main()
--- 2018/02_2.py
+++ 2018/02_2.py
@@ -1,3 +1,5 @@
+import difflib
+
 def getLines(path):
 	with open(path, 'r') as f:
 		return [line.strip() for line in f]
@@ -5,8 +7,18 @@ def getLines(path):
 def main():
 	#lines = [l.split(' ') for l in getLines('02_0'.txt)]
 	lines = getLines('02_0.txt')
-
-	print(lines)
+	#for i,s in enumerate(difflib.ndiff(a, b)):
+	for x in range(len(lines)):
+		for y in range(x, len(lines)):
+			changes = 0
+			same = []
+			for s in difflib.ndiff(lines[x], lines[y]):
+				if s[0] in ['+', '-']:
+					changes += 1
+				else:
+					same.append(s.strip())
+			if changes == 2:
+				print(''.join(same))
 
 if __name__ == '__main__':
 	main()
