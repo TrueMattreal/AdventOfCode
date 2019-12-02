e10fa8e1217ee6f89844977e0a357170c2ada19c
Mattreal <TheRealA@web.de>
2019-12-02 19:33:59 +0100

    2019 Tag 2

--- 2019/02_1.py
+++ 2019/02_1.py
@@ -1,12 +1,32 @@
 def getLines(path):
 	with open(path, 'r') as f:
-		return [line.strip() for line in f]
-	
-def main():
-	#lines = [l.split(' ') for l in getLines('02_0'.txt)]
-	lines = getLines('02_0.txt')
+		return [line.strip().split(',') for line in f][0]
 
-	print(lines)
+def main():
+	items = [int(l) for l in getLines('02_0.txt')]
+	inst = []
+	for item in items:
+		if len(inst) == 0 and item == 99:
+			break
+		if len(inst) < 4:
+			inst.append(item)
+			if len(inst) < 4:
+				continue
+		prog = inst[0]
+		param1 = items[inst[1]]
+		param2 = items[inst[2]]
+		target = inst[3]
+		print(inst)
+		if prog == 1:
+			items[target] = param1 + param2
+		elif prog == 2:
+			items[target] = param1 * param2
+		else:
+			print("mist", items)
+			exit()
+		inst = []
+
+	print(items)
 
 if __name__ == '__main__':
 	main()
--- 2019/02_2.py
+++ 2019/02_2.py
@@ -1,12 +1,1 @@
-def getLines(path):
-	with open(path, 'r') as f:
-		return [line.strip() for line in f]
-	
-def main():
-	#lines = [l.split(' ') for l in getLines('02_0'.txt)]
-	lines = getLines('02_0.txt')
-
-	print(lines)
-
-if __name__ == '__main__':
-	main()
+# bin verwirrt... daher spiele ich lieber Scythe :P
